81cc9344ef1be207b6319909d0aa650859a8dfa7
Alex Sandiiarov <sandiyarov@gmail.com>
2018-10-10 20:33:05 -0500

    Remove components from nav app

--- packages/navigation/src/App.js
+++ packages/navigation/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { navigateToUrl } from "single-spa";
-import { Button } from "components";
 
 const App = () => (
   <div>
-    <Button onClick={() => navigateToUrl("/app1")} width={100} bg="magenta">
+    <button onClick={() => navigateToUrl("/app1")} width={100} bg="magenta">
       APP1
-    </Button>
-    <Button onClick={() => navigateToUrl("/app2")} width={100} bg="blue">
+    </button>
+    <button onClick={() => navigateToUrl("/app2")} width={100} bg="blue">
       APP2
-    </Button>
+    </button>
   </div>
 );
 
